78134c426157c2565c6b701354524420dde48336
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-11-16 16:09:24 -0700

    getting stared

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+ // Created by Patricia on 11/16/17.
+ /*
+ Page Replacing Algorithms
+ Description: Simulation of Page Replacement Algorithms  to evaluate
+ the performance of memory management
+*/
+
+
+#include <iostream>
+
+
+ using namespace std;
+
+ int main() {
+
+     
+
+ }
